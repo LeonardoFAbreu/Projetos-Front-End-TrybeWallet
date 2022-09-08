2f166d0f6ed91392e5fe2724c5b96d74dc139b0a
LeonardoFAbreu <leonardofabreu@hotmail.com>
2022-09-07 21:56:14 -0300

    adiciona nova tentativa requisito 5

--- src/tests/firstTest.test.js
+++ src/tests/firstTest.test.js
@@ -4,12 +4,31 @@ import userEvent from '@testing-library/user-event';
 import renderWithRouter from './helpers/renderWith';
 import Login from '../pages/Login';
 import Wallet from '../pages/Wallet';
+import App from '../App';
 
 describe('Testing login page', () => {
+  test('if the page contains an h1 heading with the text /TrybeWallet/', () => {
+    renderWithRouter(<Login />);
+    const loginTitle = screen.getByRole('heading', { level: 1, name: /trybewallet/i });
+    expect(loginTitle).toBeInTheDocument();
+  });
+
   test('If there is a paragraph with the text /Seu e-mail/', () => {
     renderWithRouter(<Login />);
     const emailLabel = screen.getByText(/seu e-mail/i);
     expect(emailLabel).toBeInTheDocument();
+  });
+
+  test('If there is a input field below the email label', () => {
+    renderWithRouter(<Login />);
+    const emailInput = screen.getByTestId('email-input');
+    expect(emailInput).toBeInTheDocument();
+  });
+
+  test('If there is a input field below the password label', () => {
+    renderWithRouter(<Login />);
+    const passwdInput = screen.getByTestId('password-input');
+    expect(passwdInput).toBeInTheDocument();
   });
 
   test('If there is a paragraph with the text /Sua senha/', () => {
@@ -44,5 +63,21 @@ describe('Wallet page components', () => {
     const addButton = screen.getByRole('button', { name: /adicionar despesa/i });
     expect(addButton).toBeInTheDocument();
     userEvent.click(addButton);
+  });
+
+  test('if there is a table on the page', () => {
+    renderWithRouter(<Wallet />);
+    const table = screen.getByRole('table');
+    expect(table).toBeInTheDocument();
+  });
+
+  describe('Test the App component', () => {
+    test('If de /Wallet/ component is rendered', () => {
+      renderWithRouter(<App />);
+      const button = screen.getByRole('button', { name: /entrar/i });
+      userEvent.type(Email, 'email');
+      userEvent.type(Passwd, 'passwd');
+      userEvent.click(button);
+    });
   });
 });
